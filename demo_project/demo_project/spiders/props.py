e5465effa1bccbf440dd719a389c8fe38b5b3e84
Ifeanyi <ifeanyiodenigbo10@gmail.com>
2020-06-06 23:36:50 +0100

    add navigation to property webpages to extract property infor

--- demo_project/demo_project/spiders/props.py
+++ demo_project/demo_project/spiders/props.py
@@ -1,24 +1,55 @@
 import scrapy
-#import requests
+import requests
 
 class PropertySpider(scrapy.Spider):
     name = 'props'
 
-    start_urls = [
-        'https://tiganoproperties.com/'
-    ]
+    allowed_domains = ['tiganoproperties.com']
+
+    start_urls = ['https://tiganoproperties.com/']
+
+    base_url = 'https://tiganoproperties.com/'
+    propTitle = ""
+    propCostValue = ""
+    propCostType = ""
+    propImgURL = []
+    prop_url = ""
 
     def parse(self, response):
-        for prop in response.xpath("//div[@class='figure-block']"):
-            yield{
-                'propTitle': prop.xpath(".//h3[@class='pull-left']/text()").extract_first(),
-                'propCostValue': prop.xpath(".//div[@class='cap-price pull-left']/text()").extract_first(),
-                'propCostType': prop.xpath(".//a/text()").extract_first(),
-                'propImgURL': prop.xpath(".//a[@class='hover-effect']/img/@src").extract_first(),
-                'propAdd': prop.xpath(".//a[@class='hover-effect']/@href").extract_first()
-            }
 
-        next_page = response.xpath(".//a[@class='hover-effect']/img/@src").extract_first()
-        if next_page is not None:
-            next_page_link = response.urljoin(next_page)
-            yield scrapy.Request(url = next_page_link, callback = self.parse)
+        all_properties = response.xpath("//div[@class='figure-block']")
+
+        for prop in all_properties:
+            global prop_url
+            prop_url = prop.xpath(".//a[@class='hover-effect']/@href").extract_first()
+            yield scrapy.Request(prop_url, callback=self.parse_props)
+
+    def parse_props(self, response):
+        print ("New Function Called!!!")
+        global propImgURL
+        global propTitle
+        global propCostValue
+        global propCostType
+        propImgURL = []
+
+        title_div = response.xpath("//div[@class='table-cell']")
+        propTitle = title_div.xpath(".//h1/text()").extract_first()
+
+        cost_div = response.xpath("//div[@class='header-right']")
+        propCostValue = title_div.xpath(".//span[@class='item-price']/text()").extract_first()
+
+        type_li = response.xpath("//ul[@class='list-three-col']")
+        propCostType = type_li.xpath(".//li[@class='prop_status']/text()").extract_first()
+
+        all_img_div = response.xpath("//div[@class='gallery-inner']")
+        all_img_items = all_img_div.xpath("//div[@class='item']")
+        for item in all_img_items:
+            imgURL = item.xpath("./img/@src").extract_first()
+            propImgURL.append(imgURL)
+
+        yield{
+            'propTitle': propTitle,
+            'propCostValue': propCostValue,
+            'propCostType': propCostType,
+            'propImgURL': propImgURL
+        }
