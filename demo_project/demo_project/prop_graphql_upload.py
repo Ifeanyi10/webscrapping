c43c96ee11a6a189cb5402452e39edaee23e5007
Ifeanyi <ifeanyiodenigbo10@gmail.com>
2020-06-07 18:23:57 +0100

    Update the spider code

--- demo_project/demo_project/prop_graphql_upload.py
+++ demo_project/demo_project/prop_graphql_upload.py
@@ -25,7 +25,7 @@ class PropertySpider(scrapy.Spider):
             yield scrapy.Request(prop_url, callback=self.parse_props)
 
     def parse_props(self, response):
-        #print ("New Function Called!!!")
+        print ("New Function Called!!!")
         global propImgURL
         global propTitle
         global propCostValue
@@ -47,6 +47,13 @@ class PropertySpider(scrapy.Spider):
             imgURL = item.xpath("./img/@src").extract_first()
             propImgURL.append(imgURL)
 
+        if propCostType == 'For Sale':
+            propCostType = 'Sale'
+        if propCostType == 'For Rent':
+            propCostType = 'Rent'
+
+        self.upload()
+
         yield{
             'propTitle': propTitle,
             'propCostValue': propCostValue,
@@ -54,3 +61,36 @@ class PropertySpider(scrapy.Spider):
             'propImgURL': propImgURL,
             'propAdd': prop_url
         }
+
+
+    def upload(self):
+
+        query = """
+                mutation{
+                  createProperty(input:{title: %propTitle,
+                    location:{city:"",
+                      state:"Lagos"},
+                    costValue: %propCostValue,
+                    costType:%propCostType,
+                    featured:true,
+                    images: %propImgURL,
+                  description: %prop_url
+                  })
+                }
+                """
+
+        DBurl = "https://zanga-api.now.sh/"
+
+        access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyNTgyNzk3OTU2MTIxODkyMDQiLCJpYXQiOjE1OTE0ODE3NTEsImV4cCI6MTU5NDA3Mzc1MX0.K8_LWER2z4CqygDsD2_6utLKHyMwISqyY5SiA5Yc2ak"
+        headers = {
+            "token": access_token
+        }
+
+
+        request = requests.post(DBurl, json={'query': query}, headers=headers)
+        if request.status_code == 200:
+            print ("Successful")
+            #return request.json()
+        else:
+            print ("Not Successful")
+            raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
